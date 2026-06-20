8066bc75ed3859a044b57398ecd75593b586e73c
Kaal581993 <viral.prajapati.1993@gmail.com>
2026-06-20 18:06:20 +0530

    Two Pointer Traversal

--- src/main/java/patterns/traversal/TwoPointerTraversal.java
+++ src/main/java/patterns/traversal/TwoPointerTraversal.java
@@ -1,4 +1,20 @@
 package patterns.traversal;
 
 public class TwoPointerTraversal {
+    public static void twoPointerTraversal(int arr[]){
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            System.out.println(arr[left] + " " + arr[right]);
+
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        twoPointerTraversal(arr);
+    }
 }
